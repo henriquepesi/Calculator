17ec8a748c4ac672ca44de9043f5e7b771f63a91
henrique <henrique@elocriativo.com.br>
2019-04-24 23:09:59 -0300

    commit inicial enviando projeto

---
+++ App.js
@@ -0,0 +1,75 @@
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import Button from './src/components/Button';
+import Display from './src/components/Display';
+
+const initialState = {
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0, 0],
+    current: '0',
+}
+
+export default class App extends Component {
+  state = {...initialState}
+
+  addDigit = n => {
+      if(n === '.' && this.state.displayValue.includes('.')) {
+          return 
+      }
+
+      const clearDisplay = this.state.displayValue === '0'
+      || this.state.clearDisplay
+
+      const currentValue = clearDisplay ? '' : this.state.displayValue
+      const displayValue = currentValue + n
+      this.setState({displayValue, clearDisplay: false})
+  }
+
+  clearMemory = () => {
+    this.setState({ displayValue: '0'})
+  }
+
+  setOperation = operation => {
+
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Display value={this.state.displayValue} />
+        <View style={styles.buttons}>
+            <Button label="AC" triple onClick={this.clearMemory}/>
+            <Button label="/" operation onClick={this.setOperation}/>
+            <Button label="7" onClick={this.addDigit}/>
+            <Button label="8" onClick={this.addDigit}/>
+            <Button label="9" onClick={this.addDigit}/>
+            <Button label="*" operation onClick={this.setOperation}/>
+            <Button label="4" onClick={this.addDigit}/>
+            <Button label="5" onClick={this.addDigit}/>
+            <Button label="6" onClick={this.addDigit}/>
+            <Button label="-" operation onClick={this.setOperation}/>
+            <Button label="1" onClick={this.addDigit}/>
+            <Button label="2" onClick={this.addDigit}/>
+            <Button label="3" onClick={this.addDigit}/>
+            <Button label="+" operation onClick={this.setOperation}/>
+            <Button label="0" double onClick={this.addDigit}/>
+            <Button label="." onClick={this.addDigit}/>
+            <Button label="=" operation onClick={this.setOperation}/>
+        </View>
+    </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  buttons: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  }
+});
